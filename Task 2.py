1daa6910be89489b3f756df7b81070b1c049ddff
aleks.k <kurdupov.o@donnu.edu.ua>
2020-02-18 19:23:11 +0300

    Task-2

---
+++ Task 2.py
@@ -0,0 +1,31 @@
+#Write a program, which has two print statements to print the following text
+#(capital letters “O” and “H”
+print('#########', end = '\n')
+print('#',end = '\t')
+print('#', end = '\n')
+print('#',end = '\t')
+print('#', end = '\n')
+print('#',end = '\t')
+print('#', end = '\n')
+print('#',end = '\t')
+print('#', end = '\n')
+print('#########')
+print('*************************')
+print('#',end = '\t')
+print('#', end = '\n')
+
+print('#',end = '\t')
+print('#', end = '\n')
+print('#',end = '\t')
+print('#', end = '\n')
+print('#########',end = '\n')
+print('#',end = '\t')
+print('#', end = '\n')
+print('#',end = '\t')
+print('#', end = '\n')
+print('#',end = '\t')
+print('#', end = '\n')
+
+
+
+
